4cae2ed063636275064365b56d5d20e3f132984f
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,10 @@
+# How to Customize Cell Styles via Event
+
+
+<p>The following example shows how to customize the appearance of specific cells via the ASPxPivotGrid.CustomCellStyle event.</p><p>In this example, custom style settings (the orange background and bold font) are applied to a cell if it meets the following conditions:</p><p>-- It belongs to the "Quantity" data field.<br />
+-- It's not displayed within total rows or columns.<br />
+-- Its value is greater than 50.</p>
+
+<br/>
+
+
---
+++ VB/ASPxPivotGrid_CustCellsStylesViaEvents/Default.aspx.vb
@@ -0,0 +1,26 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Drawing
+Imports DevExpress.Web.ASPxPivotGrid
+Imports DevExpress.XtraPivotGrid
+
+Namespace CustomizeCellsAppearanceViaEvents
+	Partial Public Class _Default
+		Inherits System.Web.UI.Page
+		Protected Sub Page_Load(ByVal sender As Object, _
+			ByVal e As EventArgs)
+		End Sub
+		Protected Sub CustomCellStyle(ByVal sender As Object, _
+			ByVal e As PivotCustomCellStyleEventArgs)
+			If e.ColumnValueType <> PivotGridValueType.Value OrElse _
+				e.RowValueType <> PivotGridValueType.Value Then
+					Return
+			End If
+			If Convert.ToInt32(e.Value) > 50 AndAlso _
+				e.DataField.FieldName = "Quantity" Then
+					e.CellStyle.BackColor = Color.Orange
+					e.CellStyle.Font.Bold = True
+			End If
+		End Sub
+	End Class
+End Namespace
